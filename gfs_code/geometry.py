294f085f0878919eca8e071a11d06f34cb4095e5
gsnyder206 <gsnyder@stsci.edu>
2020-06-02 14:31:26 -0400

    update geometry.py

--- gfs_code/geometry.py
+++ gfs_code/geometry.py
@@ -6,30 +6,30 @@ import matplotlib
 import matplotlib.pyplot as pyplot
 import numpy as np
 import array
-import cPickle
-import asciitable
 import scipy.stats as ss
 import scipy as sp
-import astropy.io.fits as pyfits
+#import astropy.io.fits as pyfits
 import cosmocalc as cc
 import datetime
+import asciitable
+
 
 ####
 #### Name:    geometry.py
 #### Author:  Greg Snyder   gsnyder@stsci.edu
-#### Purpose:  Generates parameter setup file for mock survey fields from 
+#### Purpose:  Generates parameter setup file for mock survey fields from
 ####           continuous-volume hydrodynamical simulations using Sunrise  (Jonsson 2006).
 #### Disclaimer:  This code is provided AS-IS with absolutely NO warranty.
-####           It is largely meant as a guide rather than ideal code.  
-####           It can and should be replaced with other lightcone generation techniques. 
+####           It is largely meant as a guide rather than ideal code.
+####           It can and should be replaced with other lightcone generation techniques.
 ####           I make no claims about the immediate usability of this code.
-####           That said, I am happy to field questions and discuss issues 
+####           That said, I am happy to field questions and discuss issues
 ####           related to this code. And also to help use it.
 #### License:  ?
-#### Credit:   Users should cite Snyder et al. (2014): "Mock UDFs from Hydro Sims with Sunrise"
+#### Credit:   Users should cite Snyder et al. (2017)
 ####           AND ALSO   Kitzbichler & White 2007, Henriques et al. 2013, Overzier et al. 2013, etc.
 ####           These papers were used to create the lightcone generation algorithm below.
-####  
+####
 
 
 
@@ -48,7 +48,7 @@ class ReplicatedBox:
         self.v_ingress=v_ingress
         #maybe should define some print/convert functions for this
 
-        
+
 class LightCone:
     def __init__(self, boxSize, cosmology, name="A Lightcone"):
         self.name=name
@@ -69,11 +69,11 @@ class LightCone:
         self.dist_limit = manual_dist_limit
         if manual_dist_limit==0.0:
             self.dist_limit = self.dist_firstRep
-            
+
         self.redshift_firstRep = np.interp(self.dist_firstRep,self.cosmology.comoving_mpc_grid,self.cosmology.redshift_grid)
         self.numRep = int(self.n*self.m)
         self.x_com = np.asarray( [(self.n - 0.5/self.m)*self.L, (self.n + 0.5/self.m)*self.L] )
-        self.y_com = np.asarray( [(self.m - 0.5/self.n)*self.L, (self.m + 0.5/self.n)*self.L] ) 
+        self.y_com = np.asarray( [(self.m - 0.5/self.n)*self.L, (self.m + 0.5/self.n)*self.L] )
         self.z_com = np.asarray([self.n*self.m*self.L])
 
         self.delta_a_rad = (1.0/(self.n*self.m**2))  #small angle approx?
@@ -103,7 +103,7 @@ class LightCone:
 
         self.origin=np.asarray([0.0,0.0,0.0])
         self.snapindex = np.where(self.zlist == np.min(self.zlist))
-        
+
         self.BasicInfo()
         self.ComputeBoxes()
 
@@ -132,14 +132,14 @@ class LightCone:
 
 
     def export_runparams(self, filename,follow=False, follow_index=60, swapxy=False , swapxz=False ):
-	
+
         dirvector = 1.0*self.u3
         alpha_vector = 1.0*self.u1
         delta_vector = 1.0*self.u2
         xind=0
         yind=1
         zind=2
-		
+
         if swapxy==True:
             temp=dirvector[0]
             dirvector[0]=dirvector[1] ; dirvector[1]=temp
@@ -148,7 +148,7 @@ class LightCone:
             temp=delta_vector[0]
             delta_vector[0]=delta_vector[1] ; delta_vector[1]=temp
             xind= 1 ; yind=0 ; zind=2
-			
+
         if swapxz==True:
             temp=dirvector[0]
             dirvector[0]=dirvector[2] ; dirvector[2]=temp
@@ -157,25 +157,25 @@ class LightCone:
             temp=delta_vector[0]
             delta_vector[0]=delta_vector[2] ; delta_vector[2]=temp
             xind= 2 ; yind=1 ; zind=0
-		
 
 
-		
+
+
         f = open(filename,'w')
-        line = '## ' + self.name + ',   LightCone Created, '+ str(datetime.date.today()) + '\n' ; f.write(line) ; print line
-        line = "## Comoving Single Box L = " + str(self.L) +'\n' ; f.write(line) ; print line
-        line = "## HubbleParam = " + str(self.cosmology.H0/100.0) + '\n' ; f.write(line) ; print line ; h = self.cosmology.H0/100.0
-        line = "## Basic info: n,m = " +str( self.n) + " , " + str( self.m) + '\n' ; f.write(line) ; print line
-        line = "## Approx. Comoving distance at first repeat: " + str( round(self.dist_firstRep,6) ) + '\n' ; f.write(line) ; print line
-        line = "## Approx. Redshift at first repeat: "  + str( round(self.redshift_firstRep,6) ) + '\n' ; f.write(line) ; print line
-        line = "## Number of replications: " + str( self.numRep) + '\n' ; f.write(line) ; print line
-        line = "## del A, arcmin: {:10.5f}".format(self.delta_a_rad*(180.0/math.pi)*60.0) + '\n' ; f.write(line) ; print line
-        line = "## del B, arcmin: {:10.5f}".format(self.delta_b_rad*(180.0/math.pi)*60.0) + '\n' ; f.write(line) ; print line
-        line = "## At 0.04 arcsec/pixel, need > {:6.1f} pixels\n".format(self.square_fov_rad*(180.0/math.pi)*3600.0/0.04) ; f.write(line) ; print line
-        line = "## Direction Unit Vector: " + str( dirvector ) + '\n' ; f.write(line) ; print line
-        line = "## Alpha Unit Vector: " + str( alpha_vector ) + '\n'  ; f.write(line) ; print line
-        line = "## Delta Unit Vector: " + str( delta_vector ) + '\n' ; f.write(line) ; print line
-        line = "## Buffered Cylindricial Radius Maximum: "+str( ((self.boxlist)[-2]).cylinder_radius_approx) + '\n' ; f.write(line) ; print line
+        line = '## ' + self.name + ',   LightCone Created, '+ str(datetime.date.today()) + '\n' ; f.write(line) ; print(line)
+        line = "## Comoving Single Box L = " + str(self.L) +'\n' ; f.write(line) ; print(line)
+        line = "## HubbleParam = " + str(self.cosmology.H0/100.0) + '\n' ; f.write(line) ; print(line) ; h = self.cosmology.H0/100.0
+        line = "## Basic info: n,m = " +str( self.n) + " , " + str( self.m) + '\n' ; f.write(line) ; print(line)
+        line = "## Approx. Comoving distance at first repeat: " + str( round(self.dist_firstRep,6) ) + '\n' ; f.write(line) ; print(line)
+        line = "## Approx. Redshift at first repeat: "  + str( round(self.redshift_firstRep,6) ) + '\n' ; f.write(line) ; print(line)
+        line = "## Number of replications: " + str( self.numRep) + '\n' ; f.write(line) ; print(line)
+        line = "## del A, arcmin: {:10.5f}".format(self.delta_a_rad*(180.0/math.pi)*60.0) + '\n' ; f.write(line) ; print(line)
+        line = "## del B, arcmin: {:10.5f}".format(self.delta_b_rad*(180.0/math.pi)*60.0) + '\n' ; f.write(line) ; print(line)
+        line = "## At 0.04 arcsec/pixel, need > {:6.1f} pixels\n".format(self.square_fov_rad*(180.0/math.pi)*3600.0/0.04) ; f.write(line) ; print(line)
+        line = "## Direction Unit Vector: " + str( dirvector ) + '\n' ; f.write(line) ; print(line)
+        line = "## Alpha Unit Vector: " + str( alpha_vector ) + '\n'  ; f.write(line) ; print(line)
+        line = "## Delta Unit Vector: " + str( delta_vector ) + '\n' ; f.write(line) ; print(line)
+        line = "## Buffered Cylindricial Radius Maximum: "+str( ((self.boxlist)[-2]).cylinder_radius_approx) + '\n' ; f.write(line) ; print(line)
         line = "## Column 1:  ID#\n" ; f.write(line)
         line = "## Column 2:  Snapshot Label\n" ; f.write(line)
         line = "## Column 3:  Snapshot Redshift\n" ; f.write(line)
@@ -212,7 +212,7 @@ class LightCone:
                 followbox = (self.boxlist)[follow_index]
             if follow==False:
                 followbox=box
-                
+
             v_in_snap = followbox.v_ingress_local*1000.0*h#np.mod(box.v_ingress, self.L)*1000.0*h  #in comoving kpc h^-1 units
             v_out_snap = followbox.v_egress_local*1000.0*h #np.mod(box.v_egress, self.L)*1000.0*h
             v_in_phys = followbox.v_ingress_local*1000.0/(1.0 + box.mid_z) # in physical kpc
@@ -232,7 +232,7 @@ class LightCone:
                                                                                           v_in_phys[xind], v_in_phys[yind], v_in_phys[zind],
                                                                                           v_cam_phys[xind], v_cam_phys[yind], v_cam_phys[zind],
                                                                                           v_cam_cent_phys[xind], v_cam_cent_phys[yind], v_cam_cent_phys[zind],
-                                                                                          fov_phys, box.mid_z, RadSize_snap) ; f.write(line) ; print line
+                                                                                          fov_phys, box.mid_z, RadSize_snap) ; f.write(line) ; print(line)
             i=i+1
         f.close()
 
@@ -245,7 +245,7 @@ class LightCone:
         cmpc_from_z0 = np.interp((self.zlist)[ingress_snapindex], self.cosmology.redshift_grid, self.cosmology.comoving_mpc_grid)
 
         print("cmpc:  ", cmpc_from_z0)
-        
+
         self.boxlist.append(ReplicatedBox(self.origin,ingress_point))
         i=0
         Nvec = np.asarray([1.0,1.0,1.0])
@@ -263,7 +263,7 @@ class LightCone:
             box_i.v_egress = box_i.v_ingress + factor*self.u3  #this is where the ray leaves this box
             box_i.v_egress_local = box_i.v_egress - (Nvec-1.0)*self.L
 
-            
+
             Nvec[ind_exit[0]] = Nvec[ind_exit[0]] + 1.0  #iterate the boundary along these axes; note generically this could be - 1.0 if using arbitrary start/direction
 
 
@@ -290,7 +290,7 @@ class LightCone:
             #print closest_ind[0]
             self.boxlist.append(ReplicatedBox((Nvec-1.0)*self.L,box_i.v_egress))  #add the new box
                                                                                   #can update/save some of its basic properties after this
-            
+
             '''print i, "{:10.3f},  {:10.3f},  {:10.3f},  {:10.3f},  {:12.8f},  {:10.3f},  {:10.3f},  {:5s}".format( np.round_(distancetraveled,3),
                                                                             np.round_(self.delta_b_rad*distancetraveled,3),
                                                                             np.round_(self.delta_b_rad*np.interp(distancetraveled,self.cosmology.comoving_mpc_grid,self.cosmology.DA_mpc_grid),3),np.round_(np.interp(distancetraveled,self.cosmology.comoving_mpc_grid, self.cosmology.redshift_grid), 3), (self.L*np.round_(self.delta_b_rad*distancetraveled,3)**2)/(self.L**3),mid_dist, mid_z, (self.namelist)[closest_ind[0]])'''
@@ -322,39 +322,39 @@ if __name__=="__main__":
     delta_b_rad = (1.0/(m*n**2))
 
     skyPixel_arcsec = 0.04 #arcsec
-    print ("ideal ACS-ish scale: {:8.2f}".format(skyPixel_arcsec))
+    print("ideal ACS-ish scale: {:8.2f}".format(skyPixel_arcsec))
     Npix_A = (delta_a_rad*(180.0/math.pi)*3600.0)/skyPixel_arcsec
     Npix_B = (delta_b_rad*(180.0/math.pi)*3600.0)/skyPixel_arcsec
-    print ("Npix_A: {:10.1f}".format(Npix_A))
-    print ("Npix_B: {:10.1f}".format(Npix_B))
-	
+    print("Npix_A: {:10.1f}".format(Npix_A))
+    print("Npix_B: {:10.1f}".format(Npix_B))
+
     GB_per_slice = 4.0*Npix_A*Npix_B/1e9
     print("GigaBytes per float: {:7.2f}".format(GB_per_slice))
-	
+
     redshift = np.logspace(-3, 1, 40)
     #print z
-	
+
     Nz = (redshift.shape)[0]
-	
+
     #for zi in redshift:
 	#    res = cc.cosmocalc(zi)
 	#    print "At z= {:6.3f}, D_com= {:6.1f}; DA= {:6.1f} Mpc; DL= {:8.1f}; PS= {:3.1f} kpc/arcsec; dXz= {:5.2f}; dYz= {:5.2f}".format(
 	#    round(zi,3), res['DCMR_Mpc'], res['DA_Mpc'], res['DL_Mpc'], res['PS_kpc'], delta_a_rad*res['DCMR_Mpc'], delta_b_rad*res['DCMR_Mpc'])
 	#    test = cc.cosmocalc(2.0,H0=71.0,WM=0.27,WV=None)
-	
-	
+
+
 
     #data = asciitable.read('gfm_snaps.txt')
     #data = asciitable.read('snap_v_redshift.txt')
-	
-    data = asciitable.read('tng_snaps_v_redshift.txt')	
-	
+
+    data = asciitable.read('tng_snaps_v_redshift.txt')
+
     zlist = np.array(map(float,(data['col2'])))
-	
+
     #zlist = np.array(map(float,(data['col2'])[:315]))
     #namelist = ((data['col1'])[:315])
     #namelist = np.asarray([(s)[85:89] for s in namelist])
-	
+
     namelist = ((data['col1']))
     namelist = np.asarray([(str(s)) for s in namelist])
 
@@ -368,15 +368,15 @@ if __name__=="__main__":
     #hudf_shallow.BasicCone(5.0, 4.0, namelist, zlist)
     #hudf_narrow = LightCone(25.0/h,cosmology,"Default Deep but Narrow")
     #hudf_narrow.BasicCone(11.0, 10.0, namelist, zlist, manual_fov_arcmin=1.0)
-	
 
 
 
-    hudf_bigbox_wide = LightCone(75.0/h,cosmology,"Wide 75 Mpc")
-    hudf_bigbox_wide.BasicCone(6.0, 5.0, namelist, zlist, manual_dist_limit=10000.0)  #z~8
-    hudf_bigbox_wide.export_runparams('tng100_6_5_xyz.txt')
-    hudf_bigbox_wide.export_runparams('tng100_6_5_yxz.txt', swapxy=True)
-    hudf_bigbox_wide.export_runparams('tng100_6_5_zyx.txt', swapxz=True)
+
+    hudf_bigbox_wide = LightCone(75.0/h,cosmology,"Deep 75 Mpc")
+    hudf_bigbox_wide.BasicCone(11.0, 10.0, namelist, zlist, manual_dist_limit=10000.0)  #z~8
+    hudf_bigbox_wide.export_runparams('tng100_11_10_xyz.txt')
+    hudf_bigbox_wide.export_runparams('tng100_11_10_yxz.txt', swapxy=True)
+    hudf_bigbox_wide.export_runparams('tng100_11_10_zyx.txt', swapxz=True)
 
 
     #hudf_bigbox_vwide = LightCone(75.0/h,cosmology,"Very Wide 75mpc repeated, 136 snaps")
@@ -385,7 +385,7 @@ if __name__=="__main__":
     #hudf_bigbox_vwide.export_runparams('hudfwide_75Mpc_6_5_yxz.txt', swapxy=True)
     #hudf_bigbox_vwide.export_runparams('hudfwide_75Mpc_6_5_zyx.txt', swapxz=True)
 
-    
+
         #hudf_default.export_runparams('hudf_default_75Mpc_11_9_wrongsnaps.txt')
 
 
@@ -394,7 +394,7 @@ if __name__=="__main__":
     zgrid = cosmology.redshift_grid
 
     sizes=[75.0/0.6774,205.0/0.6774,750.0/0.6774,2000.0/0.6774]
-    
+
     print("{:6s},{:6.0f},{:6.0f},{:6.0f},{:6.0f}".format('box', sizes[0],sizes[1],sizes[2],sizes[3]))
     m = 10.0 ; n = 11.0
     print("{:6.1f},{:6.2f},{:6.2f},{:6.2f},{:6.2f}".format(1.0/(m*n**2.0)*(180.0/math.pi)*60.0,
