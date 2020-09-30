3bc56dc72239802c650b9802adb9c4e7d1627833
gsnyder206 <gsnyder@stsci.edu>
2020-09-30 20:49:11 +0000

    main three catalogs updated

--- senior_thesis/tng_lightcone_catalogs.py
+++ senior_thesis/tng_lightcone_catalogs.py
@@ -654,6 +654,6 @@ if __name__=="__main__":
     minz=0.1
     maxz=8.8
 
-    catalog_xyz = process_lightcone_catalog(lightcone="./tng300_6_5_yxz.txt",base_dir='/home/tnguser/sims.TNG/TNG300-1/output/',mag_limit=magl)
+    catalog_xyz = process_lightcone_catalog(lightcone="./tng300_6_5_zyx.txt",base_dir='/home/tnguser/sims.TNG/TNG300-1/output/',mag_limit=magl)
     catalog_xyz = catalog_xyz.process_lightcone(minz=minz,maxz=maxz)
-    catalog_xyz.output_catalog('./Lightcone_TNG300-1_mag30_6_5_yxz_GFS.txt')
+    catalog_xyz.output_catalog('./Lightcone_TNG300-1_mag30_6_5_zyx_GFS.txt')
